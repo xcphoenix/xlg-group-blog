71fd146ad1091b9c8bdcb999b829e64f0310d09d
xcphoenix <root@xcphoenix.top>
2020-01-15 16:24:25 +0800

    build(mybatis): 添加Mybatis

--- src/main/java/top/xcphoenix/groupblog/GroupblogApplication.java
+++ src/main/java/top/xcphoenix/groupblog/GroupblogApplication.java
@@ -1,5 +1,6 @@
 package top.xcphoenix.groupblog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author xuanc
  */
 @SpringBootApplication
+@MapperScan("top.xcphoenix.groupblog.mapper")
 public class GroupblogApplication {
 
     public static void main(String[] args) {
