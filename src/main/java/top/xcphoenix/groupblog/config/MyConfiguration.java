2490e084e26c048c9e99905fda6ee5ae5dbc6465
xcphoenix <root@xcphoenix.top>
2020-01-28 23:13:41 +0800

    feat(config,interceptor): 添加fastjson、处理跨域、添加拦截器

---
+++ src/main/java/top/xcphoenix/groupblog/config/MyConfiguration.java
@@ -0,0 +1,66 @@
+package top.xcphoenix.groupblog.config;
+
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import top.xcphoenix.groupblog.interceptor.LoginInterceptor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author      xuanc
+ * @date        2020/1/27 下午6:09
+ * @version     1.0
+ */
+@Configuration
+public class MyConfiguration implements WebMvcConfigurer {
+
+    /**
+     * 使用 fastJson 序列化
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastConverter.setSupportedMediaTypes(fastMediaTypes);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        converters.add(0, fastConverter);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor())
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/login");
+    }
+
+    @Bean
+    public LoginInterceptor loginInterceptor() {
+        return new LoginInterceptor();
+    }
+
+}
---
+++ src/main/java/top/xcphoenix/groupblog/interceptor/LoginInterceptor.java
@@ -0,0 +1,43 @@
+package top.xcphoenix.groupblog.interceptor;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import top.xcphoenix.groupblog.model.Result;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author      xuanc
+ * @date        2020/1/28 下午7:04
+ * @version     1.0
+ */ 
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        if (HttpMethod.OPTIONS.matches(request.getMethod()) || session.getAttribute("user") != null) {
+            return true;
+        }
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(401);
+        String resp = JSON.toJSONString(Result.error(-2, "用户未登录"));
+        response.getWriter().write(resp);
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+
+}
