7ebff54022d0aa6166e5409ad70d3681c329a67d
xcphoenix <root@xcphoenix.top>
2020-01-29 16:04:27 +0800

    feat(admin-login): 修改登录界面

--- admin/src/js/login/login.js
+++ admin/src/js/login/login.js
@@ -2,16 +2,15 @@ layui.use(['element', 'layer'], function () {
     let $ = layui.jquery;
     let layer = layui.layer;
 
-    let showMsg = function(msg) {
+    let showMsg = function (msg) {
         layer.alert(msg, {
             skin: 'layui-layer-molv'
-            ,closeBtn: 0
-            ,anim: 5
+            , closeBtn: 0
+            , anim: 5
         })
     };
 
-    // 登录事件
-    $(document).on('click', '#submit', function () {
+    $('#login-form').submit(function () {
         let username = $("input[name='username']").val();
         let password = $("input[name='password']").val();
 
@@ -33,21 +32,23 @@ layui.use(['element', 'layer'], function () {
             xhrFields: {
                 withCredentials: true
             },
-            success: function(data) {
+            success: function (data) {
                 if (data.code !== 0) {
                     showMsg(data.msg);
                 } else {
                     layer.open({
                         content: data.msg,
                     });
-                    document.cookie="uid=" + data.data;
+                    document.cookie = "uid=" + data.data;
                     $(location).attr('href', './admin.html');
                 }
             },
-            error: function(err) {
+            error: function (err) {
                 showMsg('服务器异常')
             }
-        })
+        });
+        return false;
     });
+
 
 });
